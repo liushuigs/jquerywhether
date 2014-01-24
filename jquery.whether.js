ff17562d8b960be8bb4847b429d4d1de1d20282c
xijl <jinlingxi@gmail.com>
2014-01-24 11:21:02 +0800

    add new

---
+++ jquery.whether.js
@@ -0,0 +1,44 @@
+jQuery.extend({
+    whether: function(firstParam) {
+        var sliceDeferred = [].slice;
+        var args = sliceDeferred.call(arguments, 0), 
+        i = 0, 
+        length = args.length, 
+        pValues = new Array(length), 
+        count = length, 
+        pCount = length, 
+        deferred = length <= 1 && firstParam && jQuery.isFunction(firstParam.promise) ? 
+        firstParam : 
+        jQuery.Deferred(), 
+        promise = deferred.promise();
+        function rejectFunc(i) {
+            return function(value) {
+                args[i] = arguments.length > 1 ? sliceDeferred.call(arguments, 0) : value;
+                if (!(--count)) {
+                    deferred.rejectWith(deferred,args);
+                }
+            };
+        }
+        function progressFunc(i) {
+            return function(value) {
+                pValues[i] = arguments.length > 1 ? sliceDeferred.call(arguments, 0) : value;
+                deferred.notifyWith(promise, pValues);
+            };
+        }
+        if (length > 1) {
+            for (; i < length; i++) {
+                if (args[i] && args[i].promise && jQuery.isFunction(args[i].promise)) {
+                    args[i].promise().then(deferred.resolve, rejectFunc(i), progressFunc(i));
+                } else {
+                    --count;
+                }
+            }
+            if (!count) {
+                deferred.rejectWith(deferred, args);
+            }
+        } else if (deferred !== firstParam) {
+            deferred.rejectWith(deferred, length ? [firstParam] : []);
+        }
+        return promise;
+    }
+});
